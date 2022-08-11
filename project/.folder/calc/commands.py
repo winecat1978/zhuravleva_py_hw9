3e9531b497efedeb59c46e692ae2a39f35e10407
Katherine_20 <mob1911494@gmail.com>
2022-08-11 15:13:07 +0300

    added new command

--- project/.folder/calc/commands.py
+++ project/.folder/calc/commands.py
@@ -6,7 +6,7 @@ async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Hi, {update.effective_user.first_name}!')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'/hi\n/time\n/help\n/summation(sum)\n/deduction(min)\n/multiplication(mult)\n/division(div)')
+    await update.message.reply_text(f'/hi\n/time\n/help\n\nДействия с рациональными числами\n/summation(sum)\n/deduction(min)\n/multiplication(mult)\n/division(div)\n/exponentiation(exp)')
 
 async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE): 
     await update.message.reply_text(f'{datetime.datetime.now().time()}')
@@ -42,3 +42,11 @@ async def div_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     x = float(items[1])
     y = float(items[2])
     await update.message.reply_text(f'{x}:{y} = {x/y}')
+
+async def exponent_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    print(msg)
+    items = msg.split() # /sum 123 4324
+    x = float(items[1])
+    y = float(items[2])
+    await update.message.reply_text(f'{x}^{y} = {x**y}')
