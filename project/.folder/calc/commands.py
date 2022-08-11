d854d40d533ed932cf03b190f7856f8ecb3b4a15
Katherine_20 <mob1911494@gmail.com>
2022-08-11 13:55:04 +0300

    added commands

---
+++ project/.folder/calc/commands.py
@@ -0,0 +1,44 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import datetime
+
+async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Hi, {update.effective_user.first_name}!')
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'/hi\n/time\n/help\n/summation\n/deduction\n/multiplication\n/division')
+
+async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE): 
+    await update.message.reply_text(f'{datetime.datetime.now().time()}')
+
+async def sum_command(update: Update, context: ContextTypes.DEFAULT_TYPE):   
+    msg = update.message.text
+    print(msg)
+    items = msg.split() # /sum 123 4324
+    x = float(items[1])
+    y = float(items[2])
+    await update.message.reply_text(f'{x}+{y} = {x+y}')
+
+async def minus_command(update: Update, context: ContextTypes.DEFAULT_TYPE): 
+    msg = update.message.text
+    print(msg)
+    items = msg.split() 
+    x = float(items[1])
+    y = float(items[2])
+    await update.message.reply_text(f'{x}-{y} = {x-y}')
+
+async def pow_command(update: Update, context: ContextTypes.DEFAULT_TYPE):   
+    msg = update.message.text
+    print(msg)
+    items = msg.split() # /sum 123 4324
+    x = float(items[1])
+    y = float(items[2])
+    await update.message.reply_text(f'{x}*{y} = {x*y}')
+
+async def div_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    print(msg)
+    items = msg.split() # /sum 123 4324
+    x = float(items[1])
+    y = float(items[2])
+    await update.message.reply_text(f'{x}:{y} = {x/y}')
